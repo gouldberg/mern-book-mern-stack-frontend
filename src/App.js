00e95b77fb03b4ea2ed5257fd3e99a3d0ee71ec8
gouldberg <gouldberg777@gmail.com>
2024-06-13 13:46:28 +0900

    step by step 2024/06/13

--- src/App.js
+++ src/App.js
@@ -1,3 +1,59 @@
+// ------------------------------------------------------------------------------
+// p.19
+// ------------------------------------------------------------------------------
+// import { Route, Routes, BrowserRouter } from "react-router-dom";
+// import Register from "./pages/user/register";
+// import Login from "./pages/user/login";
+// import "./App.css";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/user/register" element={<Register />} />
+//         <Route path="/user/login" element={<Login />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+// ------------------------------------------------------------------------------
+// p.87
+// ------------------------------------------------------------------------------
+// import { Route, Routes, BrowserRouter } from "react-router-dom";
+// import Register from "./pages/user/register";
+// import Login from "./pages/user/login";
+// import ReadAll from "./pages/item/readAll";
+// import ReadSingle from "./pages/item/readSingle";
+// import Create from "./pages/item/create";
+// import Update from "./pages/item/update";
+// import Delete from "./pages/item/delete";
+// import "./App.css";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/user/register" element={<Register />} />
+//         <Route path="/user/login" element={<Login />} />
+//         <Route path="/" element={<ReadAll />} />
+//         <Route path="/item/:id" element={<ReadSingle />} />
+//         <Route path="/item/create" element={<Create />} />
+//         <Route path="/item/update/:id" element={<Update />} />
+//         <Route path="/item/delete/:id" element={<Delete />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+// ------------------------------------------------------------------------------
+// p.104:  apply header + footer to all App
+// p.119:  Not Found
+// ------------------------------------------------------------------------------
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Register from "./pages/user/register";
 import Login from "./pages/user/login";
@@ -6,6 +62,7 @@ import ReadSingle from "./pages/item/readSingle";
 import Create from "./pages/item/create";
 import Update from "./pages/item/update";
 import Delete from "./pages/item/delete";
+
 import Header from "./components/header";
 import Footer from "./components/footer";
 import "./App.css";
@@ -32,3 +89,41 @@ const App = () => {
 };
 
 export default App;
+
+// ------------------------------------------------------------------------------
+// final
+// ------------------------------------------------------------------------------
+// import { Route, Routes, BrowserRouter } from "react-router-dom";
+// import Register from "./pages/user/register";
+// import Login from "./pages/user/login";
+// import ReadAll from "./pages/item/readAll";
+// import ReadSingle from "./pages/item/readSingle";
+// import Create from "./pages/item/create";
+// import Update from "./pages/item/update";
+// import Delete from "./pages/item/delete";
+// import Header from "./components/header";
+// import Footer from "./components/footer";
+// import "./App.css";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <div className="container">
+//         <Header />
+//         <Routes>
+//           <Route path="/user/register" element={<Register />} />
+//           <Route path="/user/login" element={<Login />} />
+//           <Route path="/" element={<ReadAll />} />
+//           <Route path="/item/:id" element={<ReadSingle />} />
+//           <Route path="/item/create" element={<Create />} />
+//           <Route path="/item/update/:id" element={<Update />} />
+//           <Route path="/item/delete/:id" element={<Delete />} />
+//           <Route path="*" element={<h1>Page Not Found</h1>} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
--- src/pages/item/readAll.js
+++ src/pages/item/readAll.js
@@ -1,19 +1,108 @@
+// ------------------------------------------------------------------------------
+// p.71
+// ------------------------------------------------------------------------------
+// import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+// const ReadAll = () => {
+//   const [allItems, setAllItems] = useState();
+
+//   useEffect(() => {
+//     const getAllItems = async () => {
+//       const response = await fetch("https://localhost:5000");
+//       const jsonResponse = await response.json();
+//       setAllItems(jsonResponse);
+//     };
+//     getAllItems();
+//   }, []);
+
+//   return (
+//     <div>
+//       <div>
+//         {/* <h1>すべてのアイテムデータ</h1> */}
+//         {/* {console.log(allItems)} */}
+//         {/* {allItems && allItems.allItems.map(item => console.log(item))} */}
+//         {allItems &&
+//           allItems.allItems.map((item) => (
+//             // <Link to="" key={item._id}>
+//             <Link to={`/item/${item._id}`} key={item._id}>
+//               <img src={require(`../../images${item.image}`)} alt="item" />
+//               <div>
+//                 <h2>¥{item.price}</h2>
+//                 <h3>{item.title}</h3>
+//                 <p>{item.description.substring(0, 80)}...</p>
+//               </div>
+//             </Link>
+//           ))}
+//         {/* <button onClick={getAllItems}>全データ取得</button> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ReadAll;
+
+// ------------------------------------------------------------------------------
+// p.100:  css style by className
+// ------------------------------------------------------------------------------
+// import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+// const ReadAll = () => {
+//   const [allItems, setAllItems] = useState();
+
+//   useEffect(() => {
+//     const getAllItems = async () => {
+//       const response = await fetch("https://localhost:5000");
+//       const jsonResponse = await response.json();
+//       setAllItems(jsonResponse);
+//     };
+//     getAllItems();
+//   }, []);
+
+//   return (
+//     <div>
+//       <div className="grid-container-in">
+//         {/* <h1>すべてのアイテムデータ</h1> */}
+//         {/* {console.log(allItems)} */}
+//         {/* {allItems && allItems.allItems.map(item => console.log(item))} */}
+//         {allItems &&
+//           allItems.allItems.map((item) => (
+//             // <Link to="" key={item._id}>
+//             <Link to={`/item/${item._id}`} key={item._id}>
+//               <img src={require(`../../images${item.image}`)} alt="item" />
+//               <div className="text-area">
+//                 <h2>¥{item.price}</h2>
+//                 <h3>{item.title}</h3>
+//                 <p>{item.description.substring(0, 80)}...</p>
+//               </div>
+//             </Link>
+//           ))}
+//         {/* <button onClick={getAllItems}>全データ取得</button> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ReadAll;
+
+// ------------------------------------------------------------------------------
+// p.118:  revise src in img tag
+// p.121:  change tab name
+// ------------------------------------------------------------------------------
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ReadAll = () => {
-  const [allItems, setAllIetms] = useState();
+  const [allItems, setAllItems] = useState();
 
   useEffect(() => {
-    document.title = "MERN Market";
+    document.title = "MERN MARKET";
 
     const getAllItems = async () => {
-      // const response = await fetch("http://localhost:5000")
-      const response = await fetch(
-        "https://mern-book-mern-stack-backend.onrender.com"
-      );
+      const response = await fetch("https://localhost:5000");
       const jsonResponse = await response.json();
-      setAllIetms(jsonResponse);
+      setAllItems(jsonResponse);
     };
     getAllItems();
   }, []);
@@ -21,20 +110,70 @@ const ReadAll = () => {
   return (
     <div>
       <div className="grid-container-in">
+        {/* <h1>すべてのアイテムデータ</h1> */}
+        {/* {console.log(allItems)} */}
+        {/* {allItems && allItems.allItems.map(item => console.log(item))} */}
         {allItems &&
           allItems.allItems.map((item) => (
-            <Link to={`/item/${item._id}`} key={item._id} className="card">
+            // <Link to="" key={item._id}>
+            <Link to={`/item/${item._id}`} key={item._id}>
+              {/* <img src={require(`../../images${item.image}`)} alt="item" /> */}
               <img src={item.image} alt="item" />
-              <div className="texts-area">
+              <div className="text-area">
                 <h2>¥{item.price}</h2>
                 <h3>{item.title}</h3>
                 <p>{item.description.substring(0, 80)}...</p>
               </div>
             </Link>
           ))}
+        {/* <button onClick={getAllItems}>全データ取得</button> */}
       </div>
     </div>
   );
 };
 
 export default ReadAll;
+
+// ------------------------------------------------------------------------------
+// final
+// ------------------------------------------------------------------------------
+// import { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+// const ReadAll = () => {
+//   const [allItems, setAllIetms] = useState();
+
+//   useEffect(() => {
+//     document.title = "MERN Market";
+
+//     const getAllItems = async () => {
+//       // const response = await fetch("http://localhost:5000")
+//       const response = await fetch(
+//         "https://mern-book-mern-stack-backend.onrender.com"
+//       );
+//       const jsonResponse = await response.json();
+//       setAllIetms(jsonResponse);
+//     };
+//     getAllItems();
+//   }, []);
+
+//   return (
+//     <div>
+//       <div className="grid-container-in">
+//         {allItems &&
+//           allItems.allItems.map((item) => (
+//             <Link to={`/item/${item._id}`} key={item._id} className="card">
+//               <img src={item.image} alt="item" />
+//               <div className="texts-area">
+//                 <h2>¥{item.price}</h2>
+//                 <h3>{item.title}</h3>
+//                 <p>{item.description.substring(0, 80)}...</p>
+//               </div>
+//             </Link>
+//           ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ReadAll;
